02a6e3d1186349f629d865b889add52c26b2cfc7
Steve Sloan <steve@finagle.org>
2008-05-26 11:28:54 -0700

    Added Rakefile (for tests), test stub, and basic XHTML/CSS test fixtures.

---
+++ test/test_assert_valid_asset.rb
@@ -0,0 +1,19 @@
+require 'test/unit'
+
+FixturePath = File.join File.dirname(__FILE__), 'fixtures/'
+
+class AssertValidAssetTest < Test::Unit::TestCase
+
+  def setup
+  end
+
+  def teardown
+  end
+
+  def test_valid_css
+  end
+
+  def test_valid_css
+  end
+
+end
