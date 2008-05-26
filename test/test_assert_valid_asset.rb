21c6d4466dc45b47d76602cd2a31c3c04e24bc9e
Steve Sloan <steve@finagle.org>
2008-05-26 12:13:40 -0700

    Added basic tests, fixed fixtures.

--- test/test_assert_valid_asset.rb
+++ test/test_assert_valid_asset.rb
@@ -1,19 +1,50 @@
+require 'rubygems'
+gem 'actionpack'
+
 require 'test/unit'
+require 'assert_valid_asset'
 
 FixturePath = File.join File.dirname(__FILE__), 'fixtures/'
 
 class AssertValidAssetTest < Test::Unit::TestCase
 
-  def setup
-  end
+  self.display_invalid_content = false
 
-  def teardown
-  end
-
-  def test_valid_css
+  def test_valid_markup
+    assert_valid_markup File.read( File.join( FixturePath, 'valid.xhtml' ) )
   end
 
   def test_valid_css
+    assert_valid_css File.read( File.join( FixturePath, 'valid.css' ) )
+  end
+
+  def test_invalid_markup
+    begin
+      assert_valid_markup File.read( File.join( FixturePath, 'invalid.xhtml' ) )
+
+    rescue Test::Unit::AssertionFailedError => ex
+      reasons = ex.to_s
+      assert_match /line 13: element "fnord" undefined/, reasons
+      assert_match /line 18: end tag for "div" omitted/, reasons
+
+    else
+      flunk 'Invalid markup passed validation'
+    end
+  end
+
+  def test_invalid_css
+    begin
+      assert_valid_css File.read( File.join( FixturePath, 'invalid.css' ) )
+
+    rescue Test::Unit::AssertionFailedError => ex
+      reasons = ex.to_s
+      assert_match /line 2 Unrecognized 123fnord/, reasons
+      assert_match /line 7 body Property bogosity/, reasons
+
+    else
+      flunk 'Invalid CSS passed validation'
+    end
   end
 
 end
+RAILS_ROOT = File.dirname( File.dirname(__FILE__) )
