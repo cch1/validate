216768202ec6938bd9cd19502a70a7cdc345e740
Chris Hapgood <cch1@hapgoods.com>
2009-01-27 14:28:38 -0500

    Modified to work with Rails 3.0's unstable branch

--- lib/assert_valid_asset.rb
+++ lib/assert_valid_asset.rb
@@ -1,15 +1,125 @@
 require 'action_controller/test_process'
-require 'test/unit'
+require 'action_mailer/test_case'
 require 'net/http'
-require 'md5'
+require 'digest'
 require 'ftools'
 
-class Test::Unit::TestCase
+class Validator
+  include Singleton
+
   MARKUP_VALIDATOR_HOST = ENV['MARKUP_VALIDATOR_HOST'] || 'validator.w3.org'
   MARKUP_VALIDATOR_PATH = ENV['MARKUP_VALIDATOR_PATH'] || '/check'
   CSS_VALIDATOR_HOST = ENV['CSS_VALIDATOR_HOST'] || 'jigsaw.w3.org'
-  CSS_VALIDATOR_PATH = ENV['CSS_VALIDATOR_PATH'] || '/css-validator/validator'
+  CSS_VALIDATOR_PATH = ENV['CSS_VALIDATOR_PATH'] || '/css-validator/validator'  
+  CACHE_DIR = File.join(Rails.root, 'tmp', 'validation')
 
+  class_inheritable_accessor :display_invalid_content
+  self.display_invalid_content = false
+
+  def initialize
+    File.makedirs(CACHE_DIR) unless File.exists?(CACHE_DIR)
+  end
+
+  def validate_markup(fragment, id)
+    return if validity_checks_disabled?
+    base_filename = cache_resource(id, fragment, 'html')
+
+    return unless base_filename
+    results_filename = base_filename + '.html' + '.results.yml'
+
+    begin
+      response = File.open(results_filename) do |f| Marshal.load(f) end
+    rescue
+      response = http.start(MARKUP_VALIDATOR_HOST).post2(MARKUP_VALIDATOR_PATH, "fragment=#{CGI.escape(fragment)}&output=xml")
+      File.open(results_filename, 'w+') do |f| Marshal.dump(response, f) end
+    end
+    markup_is_valid = response['x-w3c-validator-status'] == 'Valid'
+    errors = []
+    unless markup_is_valid
+      errors << XmlSimple.xml_in(response.body)['messages'][0]['msg'].collect{ |m| "Invalid markup: line #{m['line']}: #{CGI.unescapeHTML(m['content'])}" }
+    end
+    errors
+  end
+  
+  def validate_css(css, id)
+    return if validity_checks_disabled?
+    base_filename = cache_resource(id, css, 'css')
+    results_filename =  base_filename + '.css' + '.results.yml'
+    begin
+      response = File.open(results_filename) do |f| Marshal.load(f) end
+    rescue
+      params = [ 
+        file_to_multipart('file','file.css','text/css',css),
+        text_to_multipart('warning','1'),
+        text_to_multipart('profile','css2'),
+        text_to_multipart('usermedium','all') ]
+      
+      boundary = '-----------------------------24464570528145'
+      query = params.collect { |p| '--' + boundary + "\r\n" + p }.join('') + '--' + boundary + "--\r\n"
+
+      response = http.start(CSS_VALIDATOR_HOST).post2(CSS_VALIDATOR_PATH,query,"Content-type" => "multipart/form-data; boundary=" + boundary)
+      File.open(results_filename, 'w+') do |f| Marshal.dump(response, f) end
+    end
+    messages = []
+    begin
+      REXML::XPath.each( REXML::Document.new(response.body).root, "//x:tr[@class='error']", { "x"=>"http://www.w3.org/1999/xhtml" }) do |element|
+        messages << "Invalid CSS: line" + element.to_s.gsub(/<[^>]+>/,' ').gsub(/\n/,' ').gsub(/\s+/, ' ')
+      end
+    rescue REXML::ParseException
+    end
+    messages
+  end
+
+  private
+  def validity_checks_disabled?
+    ENV["NONET"] == 'true'
+  end
+
+  def text_to_multipart(key,value)
+    return "Content-Disposition: form-data; name=\"#{CGI::escape(key)}\"\r\n\r\n#{value}\r\n"
+  end
+
+  def file_to_multipart(key,filename,mime_type,content)
+    return "Content-Disposition: form-data; name=\"#{CGI::escape(key)}\"; filename=\"#{filename}\"\r\n" +
+              "Content-Transfer-Encoding: binary\r\nContent-Type: #{mime_type}\r\n\r\n#{content}\r\n"
+  end
+
+  def cache_resource(id, resource, extension)
+    resource_md5 = Digest::MD5.hexdigest(resource)
+
+    base_filename = File.join(CACHE_DIR, id)
+    filename = base_filename + ".#{extension}"
+    file_md5 = File.exists?(filename) ? File.read(filename) : nil 
+
+    if file_md5 != resource_md5
+      Dir["#{base_filename}[^.]*"].each {|f| File.delete(f)} # Remove previous results
+      File.open(filename, 'w+') do |f| f.write(resource_md5); end # Cache new resource's hash
+    end  
+    base_filename
+  end
+
+  def http
+    if Module.constants.include?("ApplicationConfig") && ApplicationConfig.respond_to?(:proxy_config)
+      Net::HTTP::Proxy(ApplicationConfig.proxy_config['host'], ApplicationConfig.proxy_config['port'])
+    else
+      Net::HTTP
+    end
+  end
+end
+
+class ActionMailer::TestCase
+  class_inheritable_accessor :display_invalid_content
+
+  def assert_valid_markup(fragment)
+    id = self.class.name.gsub(/\:\:/,'/').gsub(/Controllers\//,'') + '.' + method_name
+    message = ""
+    fragment.split($/).each_with_index{|line, index| message << "#{'%04i' % (index+1)} : #{line}#{$/}"} if display_invalid_content
+    errors = Validator.instance.validate_markup(fragment, id)
+    assert errors.empty?, message + errors.join("\n")
+  end
+end
+
+class ActionController::TestCase
   class_inheritable_accessor :display_invalid_content
   class_inheritable_accessor :auto_validate
   class_inheritable_accessor :auto_validate_excludes
@@ -18,8 +128,8 @@ class Test::Unit::TestCase
   self.display_invalid_content = false
   self.auto_validate = false
 
-  def process_with_auto_validate(action, parameters = nil, session = nil, flash = nil)
-    response = process_without_auto_validate(action,parameters,session,flash)
+  def process_with_auto_validate(action, parameters = nil, session = nil, flash = nil, http_method = "GET")
+    response = process_without_auto_validate(action,parameters,session,flash, http_method)
     if auto_validate
       return if (auto_validate_excludes and auto_validate_excludes.include?(method_name.to_sym))
       return if (auto_validate_includes and not auto_validate_includes.include?(method_name.to_sym))
@@ -51,28 +161,13 @@ class Test::Unit::TestCase
   #     assert_valid_markup
   #   end
   #
-  def assert_valid_markup(fragment=@response.body)
-    return if validity_checks_disabled?
-    base_filename = cache_resource('markup',fragment,'html')
-
-    return unless base_filename
-    results_filename =  base_filename + '-results.yml'
-
-    begin
-      response = File.open(results_filename) do |f| Marshal.load(f) end
-    rescue
-      response = http.start(MARKUP_VALIDATOR_HOST).post2(MARKUP_VALIDATOR_PATH, "fragment=#{CGI.escape(fragment)}&output=xml")
-      File.open(results_filename, 'w+') do |f| Marshal.dump(response, f) end
-    end
-    markup_is_valid = response['x-w3c-validator-status'] == 'Valid'
-    message = ''
-    unless markup_is_valid
-      fragment.split($/).each_with_index{|line, index| message << "#{'%04i' % (index+1)} : #{line}#{$/}"} if display_invalid_content
-      message << XmlSimple.xml_in(response.body)['messages'][0]['msg'].collect{ |m| "Invalid markup: line #{m['line']}: #{CGI.unescapeHTML(m['content'])}" }.join("\n")
-    end
-
-
-    assert(markup_is_valid, message)
+  def assert_valid_markup(fragment = @response.body)
+    type = @response.content_type
+    id = self.class.name.gsub(/\:\:/,'/').gsub(/Controllers\//,'') + '.' + method_name
+    message = ""
+    fragment.split($/).each_with_index{|line, index| message << "#{'%04i' % (index+1)} : #{line}#{$/}"} if display_invalid_content
+    errors = Validator.instance.validate_markup(fragment, id)
+    assert errors.empty?, message + errors.join("\n")
   end
 
   # Class-level method to quickly create validation tests for a bunch of actions at once.
@@ -106,36 +201,10 @@ class Test::Unit::TestCase
   #     assert_valid_css(File.open("#{RAILS_ROOT}/public/stylesheets/standard.css",'rb').read)
   #   end
   #
-  def assert_valid_css(css=@response.body)
-    return if validity_checks_disabled?
-    base_filename = cache_resource('css',css,'css')
-    results_filename =  base_filename + 'results.yml'
-    begin
-      response = File.open(results_filename) do |f| Marshal.load(f) end
-    rescue
-      params = [ 
-        file_to_multipart('file','file.css','text/css',css),
-        text_to_multipart('warning','1'),
-        text_to_multipart('profile','css2'),
-        text_to_multipart('usermedium','all') ]
-      
-      boundary = '-----------------------------24464570528145'
-      query = params.collect { |p| '--' + boundary + "\r\n" + p }.join('') + '--' + boundary + "--\r\n"
-
-      response = http.start(CSS_VALIDATOR_HOST).post2(CSS_VALIDATOR_PATH,query,"Content-type" => "multipart/form-data; boundary=" + boundary)
-      File.open(results_filename, 'w+') do |f| Marshal.dump(response, f) end
-    end
-    messages = []
-    begin
-      REXML::XPath.each( REXML::Document.new(response.body).root, "//x:tr[@class='error']", { "x"=>"http://www.w3.org/1999/xhtml" }) do |element|
-        messages << "Invalid CSS: line" + element.to_s.gsub(/<[^>]+>/,' ').gsub(/\n/,' ').gsub(/\s+/, ' ')
-      end
-    rescue REXML::ParseException
-    end
-    if messages.length > 0
-      message = messages.join("\n")
-      flunk("CSS Validation failed:\n#{message}")
-    end
+  def assert_valid_css(css = @response.body)
+    id = self.class.name.gsub(/\:\:/,'/').gsub(/Controllers\//,'') + '.' + method_name
+    errors = Validator.instance.validate_css(css, id)
+    assert errors.empty?, errors.join("\n")
   end
 
   # Class-level method to quickly create validation tests for a bunch of css files relative to 
@@ -154,50 +223,6 @@ class Test::Unit::TestCase
       toeval << "  assert_valid_css(File.open('#{filename}','rb').read)\n"
       toeval << "end\n"
       class_eval toeval
-    end
-  end
-
-private
-  def validity_checks_disabled?
-    ENV["NONET"] == 'true'
-  end
-
-  def text_to_multipart(key,value)
-    return "Content-Disposition: form-data; name=\"#{CGI::escape(key)}\"\r\n\r\n#{value}\r\n"
-  end
-
-  def file_to_multipart(key,filename,mime_type,content)
-    return "Content-Disposition: form-data; name=\"#{CGI::escape(key)}\"; filename=\"#{filename}\"\r\n" +
-              "Content-Transfer-Encoding: binary\r\nContent-Type: #{mime_type}\r\n\r\n#{content}\r\n"
-  end
-
-  def cache_resource(base,resource,extension)
-    resource_md5 = MD5.md5(resource).to_s
-    file_md5 = nil
-
-    output_dir = "#{RAILS_ROOT}/tmp/#{base}"
-    base_filename = File.join(output_dir, self.class.name.gsub(/\:\:/,'/').gsub(/Controllers\//,'') + '.' + method_name + '.')
-    filename = base_filename + extension
-    
-    parent_dir = File.dirname(filename) 
-    File.makedirs(parent_dir) unless File.exists?(parent_dir)
-
-    File.open(filename, 'r') do |f| 
-      file_md5 = MD5.md5(f.read(f.stat.size)).to_s
-    end if File.exists?(filename)
-
-    if file_md5 != resource_md5
-      Dir["#{base_filename}[^.]*"] .each {|f| File.delete(f)}
-      File.open(filename, 'w+') do |f| f.write(resource); end
-    end  
-    base_filename
-  end
-
-  def http
-    if Module.constants.include?("ApplicationConfig") && ApplicationConfig.respond_to?(:proxy_config)
-      Net::HTTP::Proxy(ApplicationConfig.proxy_config['host'], ApplicationConfig.proxy_config['port'])
-    else
-      Net::HTTP
     end
   end
 end
